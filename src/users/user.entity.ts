8b1c5d2c1bb7aee52e6f6824a073a4ccb232cc48
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-02-21 22:34:26 +0330

    Saving

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { AfterInsert,AfterRemove,AfterUpdate,Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 @Entity()
 export class User{
     @PrimaryGeneratedColumn()
@@ -7,4 +7,17 @@ export class User{
     email: string;
     @Column()
     password: string;
+
+    @AfterInsert()
+    logInsert() {
+        console.log('Inserted User With Id',this.id)
+    }
+    @AfterUpdate()
+    logUpdate() {
+        console.log('Updated User With Id ', this.id);
+    }
+    @AfterRemove()
+    logRemove() {
+        console.log('Removed User With Id', this.id);
+    }
 }
