ab5e45c458abd8a2d33df5e5c4fdf817ea74b66c
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-02-28 00:43:26 +0330

    Adding Update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body,Controller,Post,Get,Patch,Param,Query, Delete } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 @Controller('auth')
 export class UsersController {
@@ -23,6 +24,11 @@ export class UsersController {
     removeUser(@Param('id') id: string) {
         return this.usersService.remove(parseInt(id));
         
+    }
+
+    @Patch('/:id')
+    UpdateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.usersService.update(parseInt(id),body);
     }
 
 }
