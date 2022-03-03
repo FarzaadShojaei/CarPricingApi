46ab6f10b3e2217c076d8ff2a8bc521e94bed908
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-03-03 08:31:11 +0330

    Adding Exclude

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body,Controller,Post,Get,Patch,Param,Query, Delete,NotFoundException } from '@nestjs/common';
+import { Body,Controller,Post,Get,Patch,Param,Query, Delete,NotFoundException,UseInterceptors,ClassSerializerInterceptor } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -9,7 +9,7 @@ export class UsersController {
     createUser(@Body() body: CreateUserDto) {
         this.usersService.create(body.email, body.password);        
     }
-    
+    @UseInterceptors(ClassSerializerInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         
