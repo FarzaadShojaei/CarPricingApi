67f9c88c99fc6c3dcca44bd4f15227b766241fdf
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-03-05 23:06:38 +0330

    Omitting Exclude

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,21 +1,22 @@
+import { UserDto } from './../users/dtos/user.dto';
 import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler, ClassSerializerInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
-
+ 
 
 export class SerializerInterceptor implements NestInterceptor{
     intercept(context: ExecutionContext, handler: CallHandler<any>): Observable<any> {
-      //Run Someting before a Request Is Handled
-      //by the request Handler
-        console.log('I am Running Before The Handler', context);
+      
         
         return handler.handle().pipe(
-            map((data:any) => {
-                //Run Something Before The Response is Setout
-                console.log('I am Running Before The Response Is Set out', data);
-            })
-        )
+            map((data: any) => {
+            
+                return plainToClass(UserDto, data, {
+                    excludeExtraneousValues: true,
+                });
+            }),
+        );
     }
 
 }
