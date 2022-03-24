ba414f6e80bfabcc54976f5fa7648e98b8bda261
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-03-24 14:46:48 +0430

    Custom Params Decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,3 +1,4 @@
+
 import {
   Body,
   Controller,
@@ -16,6 +17,7 @@ import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -25,9 +27,14 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  //@Get('/whoami')
+  //whoAmI(@Session() session: any) {
+   // return this.usersService.findOne(session.userId);
+  //}
+
   @Get('/whoami')
-  whoAmI(@Session() session: any) {
-    return this.usersService.findOne(session.userId);
+  whoAmI(@CurrentUser() user: string) {
+    return user;
   }
 
   @Post('/signout')
