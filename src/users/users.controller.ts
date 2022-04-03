1698b6d0c81eab9ce3f82dae461e648ffc82982c
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-04-03 09:33:27 +0430

    Globally Scoped Interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,7 @@ import {
   Query,
   NotFoundException,
   Session,
+  
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -19,8 +20,11 @@ import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 
+import { User } from './user.entity';
+
 @Controller('auth')
 @Serialize(UserDto)
+
 export class UsersController {
   constructor(
     private usersService: UsersService,
@@ -33,7 +37,7 @@ export class UsersController {
   //}
 
   @Get('/whoami')
-  whoAmI(@CurrentUser() user: string) {
+  whoAmI(@CurrentUser() user: User) {
     return user;
   }
 
